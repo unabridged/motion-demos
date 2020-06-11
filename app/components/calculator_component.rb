e1d87adc897f6fb2a5aa9629320df9e5fbcfc92b
Drew Ulmer <drew@unabridgedsoftware.com>
2020-06-11 17:06:12 -0500

    Finish up remaining calc buttons, fix a few calc issues
    - Decimal, +/-, and percent now work
    - Buffer no longer clears when pressing an operation button
    - Decimal points are rounded off if they are not necessary (81.0 => 81)

--- app/components/calculator_component.rb
+++ app/components/calculator_component.rb
@@ -1,7 +1,7 @@
 class CalculatorComponent < ViewComponent::Base
   include Motion::Component
 
-  attr_reader :buffer, :buffering, :op, :total, :operand
+  attr_reader :buffer, :buffering, :op, :total, :operand_one, :next_number
 
   map_action :clear
 
@@ -31,55 +31,90 @@ class CalculatorComponent < ViewComponent::Base
     @total = total
     @buffering = true
     @buffer = ""
+    @next_number = false
   end
 
-  def one; @buffer << "1"; end
-  def two; @buffer << "2"; end
-  def three; @buffer << "3"; end
-  def four; @buffer << "4"; end
-  def five; @buffer << "5"; end
-  def six; @buffer << "6"; end
-  def seven; @buffer << "7"; end
-  def eight; @buffer << "8"; end
-  def nine; @buffer << "9"; end
-  def zero; @buffer << "0"; end
+  def one; add_to_buffer("1"); end
+  def two; add_to_buffer("2"); end
+  def three; add_to_buffer("3"); end
+  def four; add_to_buffer("4"); end
+  def five; add_to_buffer("5"); end
+  def six; add_to_buffer("6"); end
+  def seven; add_to_buffer("7"); end
+  def eight; add_to_buffer("8"); end
+  def nine; add_to_buffer("9"); end
+  def zero; add_to_buffer("0"); end
+  def decimal; add_to_buffer("."); end
+
+  def add_to_buffer(str)
+    if next_number
+      @buffer = str
+      @next_number = false
+    else
+      @buffer << str
+    end
+  end
 
   def add
-    @operand = @buffer.to_f
+    @operand_one = @buffer.to_f
     @op = :+
-    @buffer = ""
+    @next_number = true
   end
 
   def subtract
-    @operand = @buffer.to_f
+    @operand_one = @buffer.to_f
     @op = :-
-    @buffer = ""
+    @next_number = true
   end
 
   def multiply
-    @operand = @buffer.to_f
+    @operand_one = @buffer.to_f
     @op = :*
-    @buffer = ""
+    @next_number = true
   end
 
   def divide
-    @operand = @buffer.to_f
+    @operand_one = @buffer.to_f
     @op = :/
-    @buffer = ""
+    @next_number = true
+  end
+
+  def change_sign
+    amt = (@buffer.to_f * -1)
+    @buffer = drop_decimals(amt).to_s
+  end
+
+  def drop_decimals(num)
+    if num == num.to_i
+      num.to_i
+    else
+      num
+    end
+  end
+
+  def percent
+    amt = (@buffer.to_f / 100.0)
+    if amt == amt.to_i
+      @buffer = amt.to_i.to_s
+    else
+      @buffer = amt.to_s
+    end
   end
 
   def equals
     @total =
       case op
       when :+
-        operand + @buffer.to_f
+        operand_one + @buffer.to_f
       when :-
-        operand - @buffer.to_f
+        operand_one - @buffer.to_f
       when :*
-        operand * @buffer.to_f
+        operand_one * @buffer.to_f
       when :/
-        operand / @buffer.to_f
+        operand_one / @buffer.to_f
       end
+
+    @total = drop_decimals(@total)
 
     @buffering = false
   end
@@ -88,7 +123,8 @@ class CalculatorComponent < ViewComponent::Base
     @total = 0
     @buffering = true
     @op = nil
-    @operand = nil
+    @operand_one = nil
     @buffer = ""
+    @next_number = false
   end
 end
