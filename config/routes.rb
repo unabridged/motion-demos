e153989d9984a9547af2596cfc49fe11f2f8559f
Drew Ulmer <drew@unabridgedsoftware.com>
2020-06-22 11:05:24 -0500

    Add route for clicker game demo

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   get "/calculator", to: "demos#calculator", as: :calculator_demo
   get "/form", to: "demos#form", as: :form_demo
+  get "/clicker", to: "demos#clicker", as: :clicker_demo
 end
