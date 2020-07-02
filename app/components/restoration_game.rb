f8caeedc0b228025041c5285ace29c5b1a36701f
Lexie Love <lexie@unabridgedsoftware.com>
2020-07-02 15:27:39 -0500

    Add some styling, a new tile, and better display logic

--- app/components/restoration_game.rb
+++ app/components/restoration_game.rb
@@ -3,18 +3,25 @@ class RestorationGame < ViewComponent::Base
 
   attr_reader :selected
   attr_reader :board
+  attr_reader :index
 
   map_motion :paint
 
   def initialize(selected:)
     @selected = selected
     @board = Array.new(81, 1)
+    @index = Hash.new
+    @index[0] = "waater"
+    @index[1] = "water"
+    @index[2] = "grass"
+    @index[3] = "tree"
   end
 
   def paint(event)
     x = event.target.data["x"].to_i
     y = event.target.data["y"].to_i
-    @board[coords_to_index(x, y)] = 2
+    @selected = event.target.data["selected"].to_i
+    @board[coords_to_index(x, y)] = @selected
   end
 
   private
