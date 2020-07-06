1bdca93a59ba160b0e69e854b8a9d742d7e59a65
Drew Ulmer <drew@unabridgedsoftware.com>
2020-07-06 12:06:31 -0500

    Randomize all button colors for clicker game

--- app/helpers/clicker_game_helper.rb
+++ app/helpers/clicker_game_helper.rb
@@ -1,7 +1,18 @@
 module ClickerGameHelper
-  def score_button(amt, btn:, text: nil)
+  CLASSES = %w[
+    primary secondary success danger warning info light dark
+  ].freeze
+  BTN_CLASSES = CLASSES.flat_map do |k|
+    ["btn-#{k}", "btn-outline-#{k}"]
+  end - ["btn-outline-light"]
+
+  def score_button(amt, text: nil)
     text ||= "#{amt < 0 ? "" : "+"}#{amt}"
 
-    button_tag text, class: "btn #{btn}", data: { motion: "click", amt: amt }
+    button_tag text, class: "btn #{random_btn_class}", data: { motion: "click", amt: amt }
+  end
+
+  def random_btn_class
+    BTN_CLASSES.sample
   end
 end
