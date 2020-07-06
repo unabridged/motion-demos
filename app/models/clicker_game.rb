b67b1a933c31f57a398fa221bc010fc8068bfe57
Drew Ulmer <drew@unabridgedsoftware.com>
2020-07-06 10:09:36 -0500

    Generate a unique random key for all clicker games

--- app/models/clicker_game.rb
+++ app/models/clicker_game.rb
@@ -1,3 +1,20 @@
 class ClickerGame < ApplicationRecord
   has_many :clicker_players, dependent: :destroy
+
+  validates :key, presence: true, uniqueness: true
+
+  before_validation :set_key, on: :create
+
+  private
+
+  def generate_unique_key
+    loop do
+      key = SecureRandom.hex(5)
+      break key unless self.class.where(key: key).exists?
+    end
+  end
+
+  def set_key
+    self.key ||= generate_unique_key
+  end
 end
